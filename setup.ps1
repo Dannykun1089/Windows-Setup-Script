e1f7f1f67fa44193c2fc71ede079c4a550f1e007
Danny <dannykun1089@protonmail.com>
2022-09-07 19:00:57 +0100

    I forgot you cant enable script execution from within a script

--- setup.ps1
+++ setup.ps1
@@ -1,6 +1,3 @@
-# Enable script execution for the current user
-Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
-
 # If it isn't already, enable TLS 1.2 for secure connections
 # (This modification goes back to normal after the console exits)
 if (![System.Net.ServicePointManager]::SecurityProtocol.HasFlag([System.Net.SecurityProtocolType]::Tls12))
@@ -14,4 +11,4 @@ Start-Process Powershell.exe -Verb RunAs -ArgumentList "./setup_admin.ps1" -Wait
 Start-Process Powershell.exe -ArgumentList "./setup_user.ps1" -Wait
 
 # Reset the execution policy since i dont wanna leave any mess around
-Set-ExecutionPolicy -ExecutionPolicy Undefined
+Set-ExecutionPolicy -ExecutionPolicy Undefined -Scope CurrentUser
