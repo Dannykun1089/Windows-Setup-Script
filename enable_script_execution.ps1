9be14f5e87d9dda9b2a1219cc862e2f24ac7f644
Danny <dannykun1089@protonmail.com>
2022-09-07 18:20:34 +0100

    Removed redundant execution bypass script, added wait switches to the setup script so everything executes in order

--- enable_script_execution.ps1
+++
@@ -1,4 +0,0 @@
-Set-ExecutionPolicy RemoteSigned -Scope LocalMachine
-Write-Host "Script execution enabled for: " -NoNewline; Write-Host "Local Machine" -ForegroundColor Cyan
-Write-Host "Press enter to continue..."
-Read-Host
